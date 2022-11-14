4a4039af439af57a84bbf997f24d7a91c5d9c001
njredmitch <njredmitch12@gmail.com>
2022-11-14 13:07:53 -0500

    Created Sunflower Class

---
+++ Plants/sunflower.py
@@ -0,0 +1,26 @@
+import pygame
+from plant import Plant
+import schedule
+
+class Sunflower(Plant, pygame.sprite.Sprite):
+    path = 'PlantsVsZombies\GamePNGS\Sunflower.png'
+    cost = 50
+    sun_container = []
+    
+    def __init__(self, health, position) -> None:
+        super().__init__(health, position)
+        self._event_scheduler = schedule
+        self._event_scheduler.every(20).seconds().do(self.produce_sun())
+        self._surf = pygame.image.load(Sunflower.path).convert_alpha()
+        self._rect = pygame.rect(self.surf, self._position)
+
+    def produce_sun(self):
+        pass
+    
+    def draw(self, screen: pygame.display):
+        screen.blitz(self.rect)
+    
+
+
+
+
