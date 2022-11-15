ee3f4aed5b5af77c336efdd5c19ae35cf79cafcd
njredmitch <njredmitch12@gmail.com>
2022-11-14 20:12:50 -0500

    Created class flameshooter

---
+++ Plants/flameshooter.py
@@ -0,0 +1,19 @@
+import pygame
+from peashooter import Peashooter
+
+class Flameshooter(Peashooter, pygame.sprite.Sprite):
+    image_path = 'PlantsVsZombies\GamePNGS\Flameshooter.png'
+    _cost = 200
+
+    def __init__(self, position) -> None:
+        Peashooter.__init__(position)
+        pygame.sprite.Sprite.__init__()
+        self.image = pygame.image.load(Flameshooter.image_path).convert_alpha()
+        self.rect = self.image.get_rect(self._position)
+        
+    @staticmethod
+    def get_cost(self):
+        return Flameshooter._cost
+    
+    def shoot():
+        pass
