229f0550648c9fa41fa515bdea2f740cc21e40cf
njredmitch <njredmitch12@gmail.com>
2022-11-14 20:13:56 -0500

    Added variables image and rect, created method get_cost

--- Plants/green_peashooter.py
+++ Plants/green_peashooter.py
@@ -3,13 +3,18 @@ from peashooter import Peashooter
 
 class GreenPeashooter(Peashooter, pygame.sprite.Sprite):
     image_path = 'PlantsVsZombies\GamePNGS\Peashooter.png'
-    cost = 100
+    _cost = 100
 
     def __init__(self, position) -> None:
         Peashooter.__init__(position)
         pygame.sprite.Sprite.__init__()
-    
-    
+        self.image = pygame.image.load(GreenPeashooter.image_path).convert_alpha()
+        self.rect = self.image.get_rect(self._position)
+
+    @staticmethod
+    def get_cost(self):
+        return GreenPeashooter._cost
+
     def shoot():
         pass
 
