e3851a60c7b913a421951fec1226fa1c92f8efa2
njredmitch <njredmitch12@gmail.com>
2022-11-15 19:22:04 -0500

    variable cost -> COST

--- Plants/walnut.py
+++ Plants/walnut.py
@@ -2,13 +2,13 @@ import pygame
 from plant import Plant
 
 class Walnut(Plant, pygame.sprite.Sprite):
-    _image_path = 'PlantsVsZombies\GamePNGS\Walnut.png'
-    _cost = 75
+    image_path = 'PlantsVsZombies\GamePNGS\Walnut.png'
+    COST = 75
 
-    def __init__(self, health, position) -> None:
+    def __init__(self, position) -> None:
         Plant.__init__(400, position)
         pygame.sprite.Sprite.__init__()
-        self._image = pygame.image.load(Walnut._image_path).convert_alpha()
+        self._image = pygame.image.load(Walnut._image_path)
         self.rect = self.image.get_rect(self._position)
     
     def draw(self):
