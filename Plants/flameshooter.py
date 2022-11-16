2225bdd54d4f9339293c5c8fb816f051d54818ed
njredmitch <njredmitch12@gmail.com>
2022-11-15 19:21:10 -0500

    Fixed bug with load image, Variable cost -> COST

--- Plants/flameshooter.py
+++ Plants/flameshooter.py
@@ -3,20 +3,22 @@ from peashooter import Peashooter
 
 class Flameshooter(Peashooter, pygame.sprite.Sprite):
     image_path = 'PlantsVsZombies\GamePNGS\Flameshooter.png'
-    _cost = 200
+    COST = 200
 
     def __init__(self, position) -> None:
-        Peashooter.__init__(position)
-        pygame.sprite.Sprite.__init__()
-        self.image = pygame.image.load(Flameshooter.image_path).convert_alpha()
-        self.rect = self.image.get_rect(self._position)
+        Peashooter.__init__(self, position)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(Flameshooter.image_path)
+        self.rect = self.image.get_rect(midbottom = self._position)
         
     @staticmethod
     def get_cost():
-        return Flameshooter._cost
+        return Flameshooter.COST
 
     def shoot():
         pass
 
     def draw(self):
         pass
+
+    
