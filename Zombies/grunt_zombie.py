4fd69beb6f727a73c241e905635b7d37ce03b82a
njredmitch <njredmitch12@gmail.com>
2022-11-15 23:58:40 -0500

    imported schedule, added 2 fields, added methods to attack plants

--- Zombies/grunt_zombie.py
+++ Zombies/grunt_zombie.py
@@ -1,4 +1,6 @@
 import pygame
+import schedule
+
 from zombie import Zombie
 
 class GruntZombie(Zombie, pygame.sprite.Sprite):
@@ -9,6 +11,21 @@ class GruntZombie(Zombie, pygame.sprite.Sprite):
         pygame.sprite.Sprite(self).__init__(self)
         self.image = pygame.image.load(self.image_path)
         self.rect = self.image.get_rect(midbottom = self._position)
+        self._event_schedule = schedule
+        self._plant_to_attack = None
     
     def draw(self, display: pygame.display):
-        pass
+        display.blit(self.rect)
+
+    def attack(self, plant):
+        self._plant_to_attack = plant
+        self._event_schedule.every(5).seconds.do(self.attack_plant())
+
+    def attack_plant(self):
+        self._plant_to_attack.lose_health(self._attack_dmg)
+
+    def convert_image(self):
+        self.image.convert_alpha()
+    
+    def update_plant(self):
+        self._plant_to_attack = None
