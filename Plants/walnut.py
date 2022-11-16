2857290b81059c9a15d9ab7504732d33f019cf18
njredmitch <njredmitch12@gmail.com>
2022-11-15 23:28:35 -0500

    fixed bug with super constructor

--- Plants/walnut.py
+++ Plants/walnut.py
@@ -6,8 +6,8 @@ class Walnut(Plant, pygame.sprite.Sprite):
     COST = 75
 
     def __init__(self, position) -> None:
-        Plant.__init__(400, position)
-        pygame.sprite.Sprite.__init__()
+        Plant.__init__(self, 800, position)
+        pygame.sprite.Sprite.__init__(self)
         self._image = pygame.image.load(Walnut._image_path)
         self.rect = self.image.get_rect(self._position)
     
