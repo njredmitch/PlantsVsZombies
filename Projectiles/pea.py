14346224411e702708d24f8d718e7c6cbe218e30
njredmitch <njredmitch12@gmail.com>
2022-11-28 23:30:11 -0500

    abastracted sprite inheritance and related fields/methods to projectile superclass

--- Projectiles/pea.py
+++ Projectiles/pea.py
@@ -1,21 +1,13 @@
 from abc import ABC
 from projectile import Projectile
-import pygame
 
-class Pea(Projectile, ABC, pygame.sprite.Sprite):
+class Pea(Projectile, ABC):
 
     def __init__(self, position, dmg, path) -> None:
-        Projectile.__init__(self, position, dmg)
+        Projectile.__init__(self, position, dmg, path)
         ABC.__init__(self)
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path)
-        self.rect = self.image.get_rect(midbottom = self._position)
 
-    def draw(self, display: pygame.display):
-        display.blitz(self.rect) 
-    
-    def convert_image(self):
-        self.image.convert_alpha()
+
     
 
     
