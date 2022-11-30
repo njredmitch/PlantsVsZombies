f54c8c9a085427dc8c13b798f38585aaadfb06f0
njredmitch <njredmitch12@gmail.com>
2022-11-30 00:00:20 -0500

    removed attribute value and related methods

--- GameObjects/sun.py
+++ GameObjects/sun.py
@@ -5,12 +5,8 @@ class Sun(pygame.sprite.Sprite):
     def __init__(self, position) -> None:
         super().__init__()
         self._pos = position
-        self._value = 25
         self._image = pygame.image.load(self.image_path)
         self._rect = self.image.get_rect(mid_bottom=position)
-    
-    def get_value(self):
-        return self._value
     
     def update_position(self, x, y):
         self._pos[0] += x
