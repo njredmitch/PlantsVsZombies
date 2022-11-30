db01bfe71ae8a4d4d5eac81e19d2d63c0a96ddc1
njredmitch <njredmitch12@gmail.com>
2022-11-30 00:03:35 -0500

    imported pygame and changed container of peas

--- Plants/peashooter.py
+++ Plants/peashooter.py
@@ -1,21 +1,28 @@
 import schedule
+import pygame
 
 from abc import ABC, abstractmethod
 from plant import Plant
 
 class Peashooter(Plant, ABC):
-    peas = [[]]*5
+    peas = pygame.sprite.Group  
 
     def __init__(self, position, path) -> None:
         super().__init__(200, position, path)
         self._event_manager = schedule
+        self._primed = False
     
     def shoot_peas(self):
         self._event_manager.every(20).seconds.do(self.shoot())
+        self._primed = True
 
     def stop_shooting(self):
         self._event_manager.clear()
-        
+        self._primed = False
+
+    def get_primed(self):
+        return self._primed
+    
     @abstractmethod
     def shoot(self):
         pass
