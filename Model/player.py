838cac8c4e81a004565b2dfa4b31f14bb72ea0f7
njredmitch <njredmitch12@gmail.com>
2022-11-30 00:01:10 -0500

    added method to handle plant keys and updated store_plant_method

--- Model/player.py
+++ Model/player.py
@@ -7,7 +7,7 @@ class Player:
 
 
     def store_plant(self, plant):
-        self._plant = plant
+        self._plant = self.plant_to_store(plant)
     
     def remove_plant(self):
         self._plant = None
@@ -17,4 +17,19 @@ class Player:
     
     def gain_sun(self, val):
         self._sun += val
+    
+    def plant_to_store(self, key):
+        if key == 'FP':
+            self._plant = 'flameshooter'
+        elif key == 'IP':
+            self._plant = 'iceshooter'
+        elif key == 'GP':
+            self._plant = 'greenshooter'
+        elif key == 'W':
+            self._plant = 'walnut'
+        elif key == 'SF':
+            self._plant = 'sunflower'
+
+    def get_sun(self):
+        return self._sun
 
