187163fd9bd0c969a81f181cad2ff4013ded0e8e
njredmitch <njredmitch12@gmail.com>
2022-11-29 23:59:54 -0500

    added attribute plant

--- GameObjects/plant_shop.py
+++ GameObjects/plant_shop.py
@@ -3,14 +3,18 @@ from pygame.sprite import Sprite
 
 class PlantShop(Sprite):
 
-    def __init__(self, path, pos, cost) -> None:
+    def __init__(self, path, pos, cost, plant) -> None:
         super().__init__()
         self.image = pygame.image.load(path)
         self.rect = self.image.get_rect(midbottom = pos)
         self._cost = cost
+        self._plant = plant
     
     def get_cost(self):
         return self._cost
+    
+    def get_plant(self):
+        return self._plant
     
     
     
