9836a18e416bd67115eeefe2782bfcb24b003fc2
njredmitch <njredmitch12@gmail.com>
2022-12-01 00:09:27 -0500

    removed cost field

--- Plants/walnut.py
+++ Plants/walnut.py
@@ -3,7 +3,6 @@ from plant import Plant
 
 class Walnut(Plant, pygame.sprite.Sprite):
     image_path = 'PlantsVsZombies\GamePNGS\Walnut.png'
-    COST = 75
 
     def __init__(self, position) -> None:
         Plant.__init__(self, 800, position, self.image_path)
