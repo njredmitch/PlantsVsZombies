4bb318a9d415069b8c15b75eb58096b79e8e1a0f
njredmitch <njredmitch12@gmail.com>
2022-12-02 17:24:49 -0500

    added function that removes object after 25 seconds, simplified set_pos function

--- GameObjects/sun.py
+++ GameObjects/sun.py
@@ -1,14 +1,20 @@
 import pygame
 import random
+import sched, time
 
 class Sun(pygame.sprite.Sprite):
     image_path = 'PlantsVsZombies\GamePNGS\Sun.png'
     def __init__(self, position) -> None:
         super().__init__()
-        self._pos = position
+        self._pos = self._set_pos(position)
         self._image = pygame.image.load(self.image_path)
         self._rect = self.image.get_rect(mid_bottom=position)
-    
+        self.run_lifespan()
+        
+    def run_lifespan(self):
+        s = sched.scheduler(25, None, self.fade)
+        s.run()
+
     def update_position(self, x, y):
         self._pos[0] += x
         self._pos[1] += y
@@ -19,13 +25,12 @@ class Sun(pygame.sprite.Sprite):
     def is_clicked(self, pos):
         return self._rect.collidepoint(pos)
     
-    def set_pos(self, pos):
-        if 0 <= random.random < 0.25:
-            self._pos = (pos[0] + 20, pos[1] + 90)
-        elif 0.25 <= random.random < 0.5:
-            self._pos = (pos[0] + 20, pos[1])
-        elif 0.25 <= random.random < 0.5:
-            self._pos = (pos[0] - 20, pos[1])
-        else:
-           
-            self._pos = (pos[0] + 20, pos[1])
+    def _set_pos(self, pos):
+        p = random.sample([(pos[0] + 20, pos[1] - 90),
+                           (pos[0] + 20, pos[1]),
+                           (pos[0] - 20, pos[1] - 90),
+                           (pos[0] - 20, pos[1])])[0]
+        self._pos = p
+    
+    def fade(self):
+        self.kill()
