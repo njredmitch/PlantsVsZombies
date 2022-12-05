89d1733590365c253e66e7b48a7a4ad943369e88
njredmitch <njredmitch12@gmail.com>
2022-12-05 15:08:09 -0500

    imported pygame and added call to run game

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from Model.logic import Logic
 from View.graphics import Graphics
 from Controller.game import Game
 
+import pygame
+
+pygame.init()
+
 game_logic = Logic()
 game_graphics = Graphics(game_logic._yard, game_logic._player)
 game_controller = Game(game_logic, game_graphics)
+
+game_controller.run_game()
