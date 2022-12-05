f3df91edb80e1c618adde934781549787ece0ac5
njredmitch <njredmitch12@gmail.com>
2022-12-05 14:55:43 -0500

    added methods get_rect and __str__

--- Model/GameObjects/plant_shop.py
+++ Model/GameObjects/plant_shop.py
@@ -1,17 +1,22 @@
 import pygame
-from pygame.sprite import Sprite
 
-class PlantShop(Sprite):
+class PlantShop(pygame.sprite.Sprite):
 
-    def __init__(self, path, pos, cost, plant) -> None:
+    def __init__(self, path, pos, cost, plant):
         super().__init__()
         self.image = pygame.image.load(path)
-        self.rect = self.image.get_rect(midbottom = pos)
+        self.rect = self.image.get_rect(midleft=pos)
         self._cost = cost
         self._plant = plant
     
+    def get_rect(self):
+        return self.rect
+
     def get_cost(self):
         return self._cost
     
     def get_plant(self):
         return self._plant
+    
+    def __str__(self) -> str:
+        return f'{self._cost}'
