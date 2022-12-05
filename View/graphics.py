903740db3ce476ae628350bd818fcf3d72815973
njredmitch <njredmitch12@gmail.com>
2022-12-05 14:51:58 -0500

    added fonts and modified the draw function

--- View/graphics.py
+++ View/graphics.py
@@ -6,20 +6,32 @@ class Graphics:
 
     def __init__(self, yard :FrontYard, player : Player):
         pygame.init()
-        self._screen = pygame.display.set_mode((1200,800))
+        self._font = pygame.font.Font(None, 60)
+        self._sun_font = pygame.font.Font(None, 30)
         self._yard = yard
         self._player = player
-        self._font = pygame.font.Font(None, 60)
 
-    def draw(self):
+    def draw_graphics(self, screen : pygame.display, made):
         background = pygame.image.load('PlantsVsZombies\GamePNGS\FrontyardFull.png')
-        self._screen.blit(background, (0,0))
-        for group in self._yard.get_groups():
-            group.draw(self._screen)
+        player_surf = self._sun_font.render(f'SUN: {self._player.get_sun()}', True, 'Black')
+        made_surf = self._sun_font.render(f'Made: {made}',  True, 'Black')
 
-        if self._player.get_final_status():
-            self._screen.blit(self._font.render('YOU LOST, THE ZOMBIES ATE YOU', True, 'Red'), (300,350))
-        else:
-            self._screen.blit(self._font.render('YOU KILLED ALL THE ZOMBIES, YOU WIN!!', True, 'Black'), (200,250))
+        screen.blit(background, (0,0))
+        screen.blit(player_surf, (560,20))
+        screen.blit(made_surf, (700,20))
+        
+        self._yard.get_plants().draw(screen)
+        self._yard.get_zombies_group().draw(screen)
+        self._yard.get_projectiles().draw(screen)
+        self._yard.get_sun().draw(screen)
+        self._yard.get_game_sqaures_group().draw(screen)
+        self._yard._shop_group.draw(screen)
+        self._yard._unplaced.draw(screen)
+
+        if self._player.get_final_status() != None:
+            if not self._player.get_final_status():
+                screen.blit(self._font.render('YOU LOST, THE ZOMBIES ATE YOU', True, 'Red'), (300,350))
+            else:
+                screen.blit(self._font.render('YOU KILLED ALL THE ZOMBIES, YOU WIN!!', True, 'Black'), (200,250))
 
         pygame.display.update()
