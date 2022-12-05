965fda8914b29472534702d430c21972c1eeb243
njredmitch <njredmitch12@gmail.com>
2022-12-05 14:58:13 -0500

    added pass to most unused/buggy methods for later implementation

--- Model/GameObjects/sun.py
+++ Model/GameObjects/sun.py
@@ -7,31 +7,27 @@ class Sun(pygame.sprite.Sprite):
     def __init__(self, position) -> None:
         super().__init__()
         self._pos = self._set_pos(position)
-        self._image = pygame.image.load(self.image_path)
-        self._rect = self.image.get_rect(mid_bottom=position)
-        self.run_lifespan()
+        self.image = pygame.image.load(Sun.image_path)
+        self.rect = self.image.get_rect(midbottom=self._pos)
         
     def run_lifespan(self):
-        s = sched.scheduler(time.time, time.sleep)
-        s.add(25, None, self.fade)
-        s.run()
+        pass
 
     def update_position(self, x, y):
-        self._pos[0] += x
-        self._pos[1] += y
+        pass
     
     def update_ypos(self):
-        self._pos[1] += 1
+        pass
     
     def is_clicked(self, pos):
-        return self._rect.collidepoint(pos)
+        return self.rect.collidepoint(pos)
     
     def _set_pos(self, pos):
-        p = random.sample([(pos[0] + 20, pos[1] - 90),
-                           (pos[0] + 20, pos[1]),
-                           (pos[0] - 20, pos[1] - 90),
-                           (pos[0] - 20, pos[1])])[0]
-        self._pos = p
+        p = random.sample([(pos[0] + 40, pos[1] - 60),
+                           (pos[0] + 40, pos[1] + 30),
+                           (pos[0] - 40, pos[1] - 60),
+                           (pos[0] - 40, pos[1] + 30)], 1)[0]
+        return p
     
     def fade(self):
         self.kill()
