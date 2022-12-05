49f10c4b4c1daf51ed2174d9316def9f3819fa01
njredmitch <njredmitch12@gmail.com>
2022-12-05 15:00:17 -0500

    removed inheritance from sprite, imported copy and added deepcopy method

--- Model/Plants/sunflower.py
+++ Model/Plants/sunflower.py
@@ -1,22 +1,25 @@
 import pygame
 import schedule
+import copy
 
 from Model.Plants.plant import Plant
 from Model.GameObjects.sun import Sun
 
 
-class Sunflower(Plant, pygame.sprite.Sprite):
+class Sunflower(Plant):
     image_path = 'PlantsVsZombies\GamePNGS\Sunflower.png'
-    sun_group = pygame.sprite.Group
+    sun_group = pygame.sprite.Group()
     
     def __init__(self, position) -> None:
-        Plant.__init__(self, 200, position, self.image_path)
-        pygame.sprite.Sprite.__init__(self)
-        self._event_scheduler = schedule.default_scheduler
-        self._event_scheduler.every(20).seconds.do(self.produce_sun())
+        super().__init__(120, position, Sunflower.image_path)
+        self._event_scheduler = schedule.Scheduler()
+        self._event_scheduler.every(24).seconds.do(self.produce_sun)
 
     def produce_sun(self):
         Sunflower.sun_group.add(Sun(self._position))
     
     def run_event(self):
         self._event_scheduler.run_pending()
+    
+    def deepcopy(self):
+        return Sunflower(copy.copy(self._position))
