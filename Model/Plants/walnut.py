8facfd095cd8142024261f2bfd242b45bf92bce0
njredmitch <njredmitch12@gmail.com>
2022-12-06 15:29:17 -0500

    added method to set image

--- Model/Plants/walnut.py
+++ Model/Plants/walnut.py
@@ -6,7 +6,11 @@ class Walnut(Plant, pygame.sprite.Sprite):
     image_path = 'PlantsVsZombies\GamePNGS\Walnut.png'
 
     def __init__(self, position) -> None:
-        Plant.__init__(self, 8640, position, self.image_path)
+        Plant.__init__(self, 1440, position, self.image_path)
     
+    def set_image(self, path):
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(midbottom = self._position)
+
     def deepcopy(self):
         return Walnut(copy.copy(self._position))
